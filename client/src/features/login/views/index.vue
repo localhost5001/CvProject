f0407c08cf2bb15ae9d175ed4a4a11022b4a3158
Leo Kim <e_leokim@yahoo.com>
2022-06-14 14:21:21 -0700

    Implement login screen UI

--- client/src/features/login/views/index.vue
+++ client/src/features/login/views/index.vue
@@ -3,5 +3,32 @@
 </script>
 
 <template>
-    <div>Login screen</div>
+    <v-container class="d-flex justify-center align-center" style="height: 100vh;">
+         <v-card plain width="30vw">
+            <v-card-text class="text-center text-h5">
+                Login
+            </v-card-text>
+            <v-card-text>
+                <v-text-field 
+                    hint="User Name"
+                    label="User name"
+                    variant="outlined"
+                />
+
+                <v-text-field 
+                    hint="Password"
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                />
+
+                <v-btn block variant="contained-flat" color="primary">
+                    Login
+                </v-btn>
+                <v-btn block variant="plain">
+                    Forgot your password?
+                </v-btn>
+            </v-card-text>
+        </v-card>
+    </v-container>
 </template>
