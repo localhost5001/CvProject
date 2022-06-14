a7b885bc538dc4795217fe2e1bd5c43966135008
Leo Kim <e_leokim@yahoo.com>
2022-06-14 07:13:51 -0700

    Configure material design icons

--- client/src/vuetify.ts
+++ client/src/vuetify.ts
@@ -1,9 +1,19 @@
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, md } from 'vuetify/iconsets/md'
+
 import 'vuetify/styles'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 export const vuetify = createVuetify({
     components,
     directives,
+    icons: {
+        defaultSet: 'md',
+        aliases,
+        sets: {
+            md,
+        },
+    },
 })
